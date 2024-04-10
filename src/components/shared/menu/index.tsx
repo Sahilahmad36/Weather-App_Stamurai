ed9632231bff9ed3d2815dd3bac850b35e2f3f23
Sahil <125720378+Sahilahmad36@users.noreply.github.com>
2024-04-10 08:19:49 +0530

    Update index.tsx

--- src/components/shared/menu/index.tsx
+++ src/components/shared/menu/index.tsx
@@ -5,7 +5,6 @@ export const TopMenu = () => {
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label
-                            //@ts-ignore
                             tabIndex="0"
                             className="btn btn-ghost btn-circle"
                         >
@@ -25,7 +24,6 @@ export const TopMenu = () => {
                             </svg>
                         </label>
                         <ul
-                            //@ts-ignore
                             tabIndex="0"
                             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                         >
@@ -44,22 +42,7 @@ export const TopMenu = () => {
                     </a>
                 </div>
                 <div className="navbar-end">
-                    {/* <button className="btn btn-ghost btn-circle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button> */}
+                   
                     <button className="btn btn-ghost btn-circle">
                         <div className="indicator">
                             <svg
